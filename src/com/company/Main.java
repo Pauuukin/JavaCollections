76489a37e3169dcd484d64205df2e23287bb88be
pauuukin <oleg.paukin@mail.ru>
2019-10-08 11:30:41 +0300

    linkedList realization

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,34 +7,23 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-      /*--------- linked list ------------*/
-        List<Integer> linkedList = new LinkedList<>();
+        /*---------realisation linked list ------------*/
+        MyLinkedList myLinkedList = new MyLinkedList();
+        myLinkedList.add(1);
+        myLinkedList.add(2);
+        myLinkedList.add(10);
 
-        //  head -> [0] -> [1] -> [2] -> [3]
 
-        List<Integer> arrayList = new ArrayList<>();
+        System.out.println(myLinkedList);
+        System.out.println(myLinkedList.get(0));
+        System.out.println(myLinkedList.get(1));
+        System.out.println(myLinkedList.get(2));
 
-        //  [7][6][0][1][2][3][4][5]
+        myLinkedList.remove(2);
+        System.out.println(myLinkedList);
 
-        measureTime(linkedList);
-        measureTime(arrayList);
+        myLinkedList.remove(1);
+        System.out.println(myLinkedList);
     }
-    private static void measureTime(List<Integer> list){
-
-        long start = System.currentTimeMillis();
-
-        for(int i=0; i<100000; i++){
-            list.add(0,i); //запись в начало листа
-        }
-
-        long end = System.currentTimeMillis();
-        System.out.println(end - start);
-    }
-    /*
-    * Добавление элементов в конец листа - arrayList (в два раза)
-    * Считывание по индексу (get) - arrayList (в сотни раз быстрее)
-    * Добавление элементов в начало листа - linkedList (в сотни раз быстрее, так как не нужно сдвигать весь массив вправо)
-    * Удаление - linkedList
-    * */
 
 }
