b2314a31a7df4a60c1e6c9f06def4c950b28e5b2
pauuukin <oleg.paukin@mail.ru>
2019-10-18 12:35:15 +0300

    compare hash map, linked hash map and tree map

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,23 +10,28 @@ public class Main {
        * Использовать, когда не важен порядок и сортировка
        *
        * */
-        Map<Integer, String> map = new HashMap<>();
+        Map<Integer, String> hashMap = new HashMap<>(); //внутри не гарантируется порядка
+        Map<Integer, String> linkedHashMap = new LinkedHashMap<>(); // в каком порядке добавляли пары, в таком они и вернутся
+        Map<Integer, String> treeMap = new TreeMap<>(); //пары будут отсортированы по ключу (естественный порядок)
 
-        map.put(1, "Один");         //Entry
-        map.put(2, "Два");          //Entry
-        map.put(3, "Три");          //Entry
+        testMap(treeMap);
 
-        System.out.println(map);
-        // не может быть дубликатов ключей
-        map.put(3,"-три-");
-        System.out.println(map);
-        System.out.println(map.get(1));
 
         System.out.println("----------------------------------");
 
-        for(Map.Entry<Integer,String> entry : map.entrySet()) {
-            System.out.println(entry.getKey() + " : " + entry.getValue());
         }
-    }
+        public static void  testMap(Map<Integer, String> map)
+        {
+            map.put(39, "Bob");
+            map.put(22, "Mike");
+            map.put(2, "Phill");
+            map.put(49, "Sam");
+            map.put(9, "Frodo");
+            map.put(280, "Leo");
 
+            for (Map.Entry<Integer, String> entry : map.entrySet()) {
+                System.out.println(entry.getKey() + " : " + entry.getValue());
+
+            }
+        }
 }
