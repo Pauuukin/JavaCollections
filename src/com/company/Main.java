a1b7d8c0b7cd57181a6cd5d3d49efcb39ed18c3b
pauuukin <oleg.paukin@mail.ru>
2019-10-18 14:27:35 +0300

    Set begin

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,33 +5,76 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-       /*-------- hash map ------------
-       *
-       * Использовать, когда не важен порядок и сортировка
+       /*-------- Set ------------
+       * хрнаят обычные объекты, как и list, но тут нельзя хранить одинаковые элементы
        *
        * */
-        Map<Integer, String> hashMap = new HashMap<>(); //внутри не гарантируется порядка
-        Map<Integer, String> linkedHashMap = new LinkedHashMap<>(); // в каком порядке добавляли пары, в таком они и вернутся
-        Map<Integer, String> treeMap = new TreeMap<>(); //пары будут отсортированы по ключу (естественный порядок)
+        Set<String> hashSet = new HashSet<>();
+        Set<String> linkedHashSet = new LinkedHashSet<>();
+        Set<String> treeSet = new TreeSet<>();
 
-        testMap(treeMap);
+        hashSet.add("Mike");
+        hashSet.add("Tom");
+        hashSet.add("Ford");
+        hashSet.add("Leo");
+        hashSet.add("Max");
+        //пробуем добавть дубликаты
+        hashSet.add("Tom");
+        hashSet.add("Tom");
 
 
-        System.out.println("----------------------------------");
+        //вхождение элемента в set
+        System.out.println(hashSet.contains("Tom"));
+        System.out.println(hashSet.contains("Tim"));
+        //пустой или нет
+        System.out.println(hashSet.isEmpty());
 
+        for (String name : hashSet){
+            System.out.println(name);
         }
-        public static void  testMap(Map<Integer, String> map)
-        {
-            map.put(39, "Bob");
-            map.put(22, "Mike");
-            map.put(2, "Phill");
-            map.put(49, "Sam");
-            map.put(9, "Frodo");
-            map.put(280, "Leo");
 
-            for (Map.Entry<Integer, String> entry : map.entrySet()) {
-                System.out.println(entry.getKey() + " : " + entry.getValue());
+        System.out.println(hashSet);
 
-            }
-        }
+        System.out.println("---------------------------------");
+
+        Set<Integer> set1 = new HashSet<>();
+        set1.add(0);
+        set1.add(1);
+        set1.add(2);
+        set1.add(3);
+        set1.add(4);
+        set1.add(5);
+
+        Set<Integer> set2 = new HashSet<>();
+        set2.add(2);
+        set2.add(3);
+        set2.add(4);
+        set2.add(5);
+        set2.add(6);
+        set2.add(7);
+        set2.add(8);
+
+        //union - объединение множеств
+        Set<Integer> union = new HashSet<>(set1);
+        union.addAll(set2);
+
+        System.out.println(union);
+
+        // intersection - пересечение множеств
+        Set<Integer> intersection = new HashSet<>(set1);
+        intersection.retainAll(set2);
+
+        System.out.println(intersection);
+
+        // difference - разность множеств
+        Set<Integer> difference = new HashSet<>(set1);
+        difference.removeAll(set2);
+
+        System.out.println(difference);
+
+    }
+
+
+
+
 }
